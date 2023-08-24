8aecdb31d8ebdf1bc121454d621f128fd3cebf50
ilayc11 <ilayc11@gmail.com>
2023-08-24 14:34:02 +0300

    us states game

--- 100 Days Of Python/Simple Programs/usStatesGame/stateText.py
+++
@@ -1,9 +0,0 @@
-from turtle import Turtle
-class stateText(Turtle):
-    def __init__(self,name,x,y):
-        super().__init__()
-        self.color("black")
-        self.penup()
-        self.hideturtle()
-        self.goto(x,y)
-        self.write(name)
