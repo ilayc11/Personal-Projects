a6623afbcf2c2286e956307729bbf84378c7cfc5
ilayc11 <ilayc11@gmail.com>
2023-09-06 20:24:49 +0300

    Miles 2 Km with GUI

--- 100 Days Of Python/Simple Programs/Mile2Kilometers/main.py
+++ 100 Days Of Python/Simple Programs/Mile2Kilometers/main.py
@@ -1,13 +1,32 @@
-import tkinter
+from tkinter import *
 
-window = tkinter.Tk()
+window = Tk()
 
-window.title("Miles 2 Kilometers")
-window.minsize(width=500,height=300)
+window.title("Miles 2 Kilometers Converter")
+window.config(padx=20,pady=20)
 
-my_label=tkinter.Label(text="My Label",font=("Ariel",24,"bold"))
-my_label.pack(side="left",expand=True)
+firstLabel = Label(text="is equal to")
+firstLabel.grid(column=0, row=1)
+
+kmLabel = Label(text="Km")
+kmLabel.grid(column=2, row=1)
+
+milesLabel = Label(text="Miles")
+milesLabel.grid(row=0, column=2)
+
+textBox = Entry()
+textBox.grid(row=0, column=1)
+
+numbyy = 0
+
+converted = Label(text=numbyy)
+converted.grid(row=1, column=1)
 
 
+def buttCommand():
+    converted.config(text=(float(textBox.get())*1.689))
 
+
+button = Button(text="calculate", command=buttCommand)
+button.grid(row=3,column=1)
 window.mainloop()
